6c47e5b0be92d0ab45a77806f0e41b2775c660e8
Rudolf Markulin <rudolf.markulin@protonmail.com>
2018-11-05 17:38:58 +0100

    fix parsing map2

--- src/main/scala/challenge.scala
+++ src/main/scala/challenge.scala
@@ -5,7 +5,7 @@ case class Coordinate(x: Int, y:Int) {
   def down  = this.copy(y = this.y + 1)
   def left  = this.copy(x = this.x - 1)
   def right = this.copy(x = this.x + 1)
-  def valid = this.x >= 0 && this.y >= 0
+  def valid(maxX: Int, maxY: Int): Boolean = this.x >= 0 && this.y >= 0 && this.x < maxX && this.y < maxY
 }
 
 object Challenge extends App {
@@ -16,6 +16,16 @@ object Challenge extends App {
     Array("x", "-", "B", "-", "+", "" , "" , "" , "C"),
     Array("" , "" , "" , "" , "|", "" , "" , "" , "|"),
     Array("" , "" , "" , "" , "+", "-", "-", "-", "+"),
+  )
+
+  val map2 = Array(
+    Array("@", "" , "" , "" , "" , "" , "" , "" , "" , "" ),
+    Array("|", "" , "C", "-", "-", "-", "-", "+", "" , "" ),
+    Array("A", "" , "|", "" , "" , "" , "" , "|", "" , "" ),
+    Array("+", "-", "-", "-", "B", "-", "-", "+", "" , "" ),
+    Array("" , "" , "|", "" , "" , "" , "" , "" , "" , "x"),
+    Array("" , "" , "|", "" , "" , "" , "" , "" , "" , "|"),
+    Array("" , "" , "+", "-", "-", "-", "D", "-", "-", "+"),
   )
 
 
@@ -35,30 +45,29 @@ object Challenge extends App {
 
     var path = List[(String, Coordinate)]()
 
+    val maxY = map.length
+    val maxX = map.head.length
+
     while (current._1 != "x") {
       val possibleNeighbours = current._1 match  {
-        case "@" => {
-            List(current._2.up, current._2.down, current._2.left, current._2.right)
-        }
-        case "-" => {
-            List(current._2.left, current._2.right)
-        }
-        case "|" => {
-            List(current._2.up, current._2.down)
-        }
+        case "@" => List(current._2.up, current._2.down, current._2.left, current._2.right)
+        case "-" => if(previous._1 == "|") List(current._2.up, current._2.down) else List(current._2.left, current._2.right)
+        case "|" => List(current._2.up, current._2.down)
         case "+" => {
-            (if(previous._1 == "-") List(current._2.up, current._2.down)
+            if(previous._1 == "-") List(current._2.up, current._2.down)
             else if (previous._1 == "|") List(current._2.left, current._2.right)
-            else List())
+            else if (previous._2.y == current._2.y + 1) List(current._2.left, current._2.right)
+            else if (previous._2.y == current._2.y - 1) List(current._2.left, current._2.right)
+            else if (previous._2.x == current._2.x + 1) List(current._2.up, current._2.down)
+            else if (previous._2.x == current._2.x - 1) List(current._2.up, current._2.down)
+            else Nil //todo
         }
         case normal: String => {
-            (if(previous._1 == "-") List(current._2.left, current._2.right)
-            else if (previous._1 == "|") List(current._2.up, current._2.down)
-            else List())
+            List(current._2.up, current._2.down, current._2.left, current._2.right)
         }
       }
       val next = possibleNeighbours
-        .filter(_.valid)
+        .filter(_.valid(maxX, maxY))
         .filter(_ != previous._2)
         .find(n => map(n.y)(n.x) != "")
         .get //todo check for multiple
@@ -72,5 +81,6 @@ object Challenge extends App {
   }
 
   println(solve(map1).reverse.map(_._1))
+  println(solve(map2).reverse.map(_._1))
 
 }
